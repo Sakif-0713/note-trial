b821d2dac832b9f894818184b56127934c20b060
Sakif-0713 <moktadirskf1@gmail.com>
2024-10-16 14:01:41 +0600

    2nd cotton

---
+++ horta.java
@@ -0,0 +1,10 @@
+package horiz;
+
+public class horta {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("The value of N: ");
+        int N = scanner.nextInt();
+}
